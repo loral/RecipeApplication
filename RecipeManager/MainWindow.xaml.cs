9e13cd79cfb595c2e5314ce43cb0bbe1a29774ad
Loral <loral_godfrey@yahoo.com>
2015-01-16 13:49:01 -0700

    Adding File Menu

--- RecipeManager/MainWindow.xaml.cs
+++ RecipeManager/MainWindow.xaml.cs
@@ -25,9 +25,58 @@ namespace RecipeManager
             InitializeComponent();
         }
 
+        #region Application Logic
+
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             //Any stuff that needs to be set or loaded after the window is created needs to go here.
         }
+
+        private void AddRecipe(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("You clicked 'Add Recipe'");
+        }
+
+        private void ViewRecipe(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("You clicked 'View Recipe'");
+        }
+
+        private void CreateMenu(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("You clicked 'Create Menu'");
+        }
+
+        private void Open(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("You clicked 'Open'");
+        }
+
+        private void Save(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("You clicked 'Save'");
+        }
+
+        private void SaveAs(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("You clicked 'Save As'");
+        }
+
+        private void Exit(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("You clicked 'Exit'");
+        }
+
+        private void Tools(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("You clicked 'Tools'");
+        }
+
+        private void Help(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("You clicked 'Help'");
+        }
+
+        #endregion
     }
 }
---
+++ RecipeManager/Recipe.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeManager
+{
+    public class Recipe
+    {
+        public string name;
+        public string prepTime;
+        public string cookTime;
+        public List<Ingredient> ingredients;
+        public List<string> directions;
+        public string yeild;
+        public enum meal { Breakfast, Lunch, Dinner };
+        public enum recipeType { MainDish, Side, Dessert, Beverage };
+    }
+}
