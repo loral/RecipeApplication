c83fe4ff16abaa505127a907e15b3e3c27681724
Loral <loral_godfrey@yahoo.com>
2015-02-13 11:35:43 -0700

    ingredient filter back to textbox

--- RecipeManager/obj/Release/RecipeManager_Content.g.i.cs
+++ RecipeManager/obj/Release/RecipeManager_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("hat4.ico")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("hat5.ico")]
 
 
