013535b3d741cdb0da0d796bc74db2d03606a0f5
Loral <loral_godfrey@yahoo.com>
2015-02-15 16:15:57 -0700

    Save verbiage

--- RecipeManager/MainWindow.xaml.cs
+++ RecipeManager/MainWindow.xaml.cs
@@ -306,7 +306,7 @@ namespace RecipeManager
             // Configure open file dialog box
             Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
             dlg.DefaultExt = ".rmn"; // Default file extension
-            dlg.Filter = "Recipe Manager documents (.rmn)|*.rmn"; // Filter files by extension 
+            dlg.Filter = "Recipe Manager (.rmn)|*.rmn"; // Filter files by extension 
 
             // Show open file dialog box
             Nullable<bool> result = dlg.ShowDialog();
